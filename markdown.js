563992a9e4fcdffc079905462a982f5190b6fe52
ruffy <ruffy.developer@gmail.com>
2020-04-25 06:24:56 +0200

    Add syntax highlighting for code.

--- markdown.js
+++ markdown.js
@@ -4,6 +4,7 @@ const standardIn = require('./lib/standardIn');
 const lib = require('./lib/markdown');
 
 
+
 standardIn.withAllRead((content) => {
     const renderedContent = lib.render(content);
     console.log(renderedContent)
