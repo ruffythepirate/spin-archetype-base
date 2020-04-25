9288f35002d1b56a60fda99058d5672d65199764
ruffy <ruffy.developer@gmail.com>
2020-04-25 07:05:22 +0200

    Fix forgotten shebang in jqake and JSON.stringify in output-helper.

--- jqtake.js
+++ jqtake.js
@@ -1,14 +1,12 @@
+#! /usr/bin/env node
 const standardIn = require('./lib/standardIn');
 const jqtake = require('./lib/jqtake');
-
 const output = require('./lib/common/output-helper');
 
-
 standardIn.withAllObjects((objects) => {
-
     args = jqtake.parseArguments();
 
     objects = objects.splice(0, args.number);
 
     output.outputJson(objects);
-})
+});
