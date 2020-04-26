2c97f95c24c3cf26460d411e2614519fdd7329a5
ruffythepirate <ruffy.developer@gmail.com>
2020-04-26 07:56:35 +0200

    Fix bug in create-link-tag function

--- create-link-tag.js
+++ create-link-tag.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const standardIn = require('./lib/standardIn');
 
-standardIn.withAllRead((allContent) => {
-    console.log(`<link rel="stylesheet" href="${allContent}"/>`);
-})
+standardIn.forEachLine((line) => {
+    console.log(`<link rel="stylesheet" href="${line}"/>`);
+});
